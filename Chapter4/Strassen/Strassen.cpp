ec3d0756ac4134ca9f1f5a306a91e560a9c4ed46
WhatTheNathan <nathanliuyolo@gmail.com>
2018-02-11 13:10:02 +0800

    Strassen

--- Chapter4/Strassen/Strassen.cpp
+++ Chapter4/Strassen/Strassen.cpp
@@ -13,6 +13,7 @@ vector<vector<int>> matrix_minus(vector<vector<int>> A,vector<vector<int>> B) {
             C[i][j] = A[i][j] - B[i][j];
         }
     }
+    return C;
 }
 
 vector<vector<int>> matrix_plus(vector<vector<int>> A,vector<vector<int>> B) {
@@ -25,6 +26,7 @@ vector<vector<int>> matrix_plus(vector<vector<int>> A,vector<vector<int>> B) {
             C[i][j] = A[i][j] + B[i][j];
         }
     }
+    return C;
 }
 
 vector<vector<int>> matrix_multiply(vector<vector<int>> A,vector<vector<int>> B) {
@@ -38,6 +40,7 @@ vector<vector<int>> matrix_multiply(vector<vector<int>> A,vector<vector<int>> B)
             for(int k = 0; k < A[0].size(); k++)
                 C[i][j] += A[i][k] * B[k][j];
         }
+    return C;
 }
 
 vector<vector<int>> Strassen(vector<vector<int>> A,vector<vector<int>> B) {
@@ -51,51 +54,33 @@ vector<vector<int>> Strassen(vector<vector<int>> A,vector<vector<int>> B) {
     else {
         /* A partion */
         vector<vector<int>> A_11(n/2,vector<int>(n/2));
-        for(int i = 0; i < n / 2; i++)
-            for(int j = 0; j < n / 2; j++)
-                A_11[i][j] = A[i][j];
-
         vector<vector<int>> A_12(n/2,vector<int>(n/2));
-        for(int i = n / 2; i < n; i++)
-            for(int j = 0; j < n / 2; j++)
-                A_12[i][j] = A[i][j];
-
         vector<vector<int>> A_21(n/2,vector<int>(n/2));
-        for(int i = 0; i < n; i++)
-            for(int j = n / 2; j < n; j++)
-                A_21[i][j] = A[i][j];
-
         vector<vector<int>> A_22(n/2,vector<int>(n/2));
-        for(int i = n / 2; i < n; i++)
-            for(int j = n / 2; j < n; j++)
-                A_22[i][j] = A[i][j];
 
-        /* B partion */
         vector<vector<int>> B_11(n/2,vector<int>(n/2));
-        for(int i = 0; i < n / 2; i++)
-            for(int j = 0; j < n / 2; j++)
-                B_11[i][j] = A[i][j];
-
         vector<vector<int>> B_12(n/2,vector<int>(n/2));
-        for(int i = n / 2; i < n; i++)
-            for(int j = 0; j < n / 2; j++)
-                B_12[i][j] = A[i][j];
-
         vector<vector<int>> B_21(n/2,vector<int>(n/2));
-        for(int i = 0; i < n; i++)
-            for(int j = n / 2; j < n; j++)
-                B_21[i][j] = A[i][j];
-
         vector<vector<int>> B_22(n/2,vector<int>(n/2));
-        for(int i = n / 2; i < n; i++)
-            for(int j = n / 2; j < n; j++)
-                B_22[i][j] = A[i][j];
 
-        /* C partion */
         vector<vector<int>> C_11(n/2,vector<int>(n/2));
         vector<vector<int>> C_12(n/2,vector<int>(n/2));
         vector<vector<int>> C_21(n/2,vector<int>(n/2));
         vector<vector<int>> C_22(n/2,vector<int>(n/2));
+
+        for(int i = 0; i < n / 2; i++) {
+            for(int j = 0; j < n / 2; j++) {
+                A_11[i][j] = A[i][j];
+                A_12[i][j] = A[i][j + n / 2];
+                A_21[i][j] = A[i + n / 2][j];
+                A_22[i][j] = A[i + n / 2][j + n / 2];
+
+                B_11[i][j] = B[i][j];
+                B_12[i][j] = B[i][j + n / 2];
+                B_21[i][j] = B[i + n / 2][j];
+                B_22[i][j] = B[i + n / 2][j + n / 2];
+            }
+        }
 
         /* S */
         vector<vector<int>> S_1 = matrix_minus(B_12,B_22);
@@ -110,14 +95,43 @@ vector<vector<int>> Strassen(vector<vector<int>> A,vector<vector<int>> B) {
         vector<vector<int>> S_10 = matrix_plus(B_11,B_12);
 
         /* P */
-        vector<vector<int>> P_1 = matrix_multiply(B_12,B_22);
+        vector<vector<int>> P_1 = matrix_multiply(A_11,S_1);
+        vector<vector<int>> P_2 = matrix_multiply(S_2,B_22);
+        vector<vector<int>> P_3 = matrix_multiply(S_3,B_11);
+        vector<vector<int>> P_4 = matrix_multiply(A_22,S_4);
+        vector<vector<int>> P_5 = matrix_multiply(S_5,S_6);
+        vector<vector<int>> P_6 = matrix_multiply(S_7,S_8);
+        vector<vector<int>> P_7 = matrix_multiply(S_9,S_10);
+
+        /* C */
+        C_11 = matrix_plus(matrix_minus(matrix_plus(P_5,P_4),P_2),P_6);
+        C_12 = matrix_plus(P_1,P_2);
+        C_21 = matrix_plus(P_3,P_4);
+        C_22 = matrix_minus(matrix_minus(matrix_plus(P_5,P_1),P_3),P_7);
+
+        for(int i = 0; i < n / 2; i++) {
+            for(int j = 0; j < n / 2; j++) {
+                C[i][j] = C_11[i][j];
+                C[i][j + n / 2] = C_12[i][j];
+                C[i + n / 2][j] = C_21[i][j];
+                C[i + n / 2][j + n / 2] = C_22[i][j];
+            }
+        }
     }
     return C;
 }
 
-int main(){
-    vector<vector<int>> A = {{1,2},{2,1}};
-    vector<vector<int>> B = {{1,2},{2,1}};
+int main() {
+    vector<vector<int>> A = {{1,2,3,4},{2,1,3,4},{1,2,3,4},{1,2,3,4}};
+    vector<vector<int>> B = {{1,2,3,4},{2,1,3,4},{1,2,3,4},{1,2,3,4}};
     vector<vector<int>> C;
-    C = Strassen(A,B);
+    C = Strassen(A, B);
+
+//    C= matrix_multiply(A,B);
+    for(int i = 0; i < A.size(); i++) {
+        for (int j = 0; j < B[0].size(); j++) {
+            cout << C[i][j] << " ";
+        }
+        cout<<endl;
+    }
 }
