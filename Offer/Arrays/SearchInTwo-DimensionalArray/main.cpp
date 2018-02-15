721c7dfdce445a7fd8e04d08d8add3384b0b5b2b
WhatTheNathan <nathanliuyolo@gmail.com>
2018-02-15 17:39:36 +0800

    SearchInTwo-DimensionalArray

--- Offer/Arrays/SearchInTwo-DimensionalArray/main.cpp
+++ Offer/Arrays/SearchInTwo-DimensionalArray/main.cpp
@@ -8,6 +8,29 @@
 
 using namespace::std;
 
-int main(){
+bool find(int *matrix, int rows, int columns, int number) {
+    if (matrix != nullptr && rows > 0 && columns > 0) {
+        int row = 0;
+        int column = columns - 1;
+        while(row < rows && column >= 0) {
+            if (matrix[row * columns + column] == number) {
+                return true;
+            } else if (matrix[row * columns + column] > number) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+    }
+    return false;
+}
 
+int main(){
+    int matrix[] = {1,2,8,9,
+                    2,4,9,12,
+                    4,7,10,13,
+                    6,8,11,15};
+    cout<<find(matrix,4,4,7)<<endl;
+    cout<<find(matrix,4,4,20)<<endl;
+    cout<<find(nullptr,4,4,20)<<endl;
 }
