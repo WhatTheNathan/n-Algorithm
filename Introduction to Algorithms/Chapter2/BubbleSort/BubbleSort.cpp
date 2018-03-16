f09b71efebfb02cd0fc89285c6f9ce7993acebdc
WhatTheNathan <nathanliuyolo@gmail.com>
2018-03-16 18:49:43 +0800

    bubbleSort

---
+++ Introduction to Algorithms/Chapter2/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace::std;
+
+void bubbleSort(int* number, int length) {
+    if(!number)
+        return;
+    for(int i = 0; i < length; i++) {
+        for(int j = 0; j < length - i -1; j++) {
+            if(number[j] > number[j + 1]) {
+                int temp = number[j];
+                number[j] = number[j + 1];
+                number[j + 1] = temp;
+            }
+        }
+        for(int i = 0; i < length; i++)
+            cout<<number[i]<<" ";
+        cout<<endl;
+    }
+}
+
+int main(){
+    const int length = 9;
+    int number[length] = {9,8,7,6,5,4,3,2,1};
+    bubbleSort(number,length);
+    for(int i = 0; i < length; i++)
+        cout<<number[i]<<" ";
+    cout<<endl;
+}
+
