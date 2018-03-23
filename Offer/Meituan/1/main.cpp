35c4cf397c70bcedefd09d1c1d690f22b4f4742e
WhatTheNathan <nathanliuyolo@gmail.com>
2018-03-23 19:57:38 +0800

    Meituan_1

--- Offer/Meituan/1/main.cpp
+++ Offer/Meituan/1/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdlib>
+#include <cstring>
 
+/*
+ * 给出两个相同长度的由字符a和b构成的字符串，定义它们的距离为对应位置不同的字符的数量。
+ * 如串"aab"与串"aba"的距离为2; 下面给出两个字符串S与T，其中S的长度不小于T的长度。
+ * 我们用|S|代表S的长度，|T|代表T的长度，那么在S中一共有|S|-|T|+1个与T长度相同的子串，现在你需要计算T串与这些|S|-|T|+1个子串的距离的和。
+ */
 using namespace::std;
 
 int getDistance(string S, string T) {
@@ -18,13 +25,10 @@ vector<string> subString(string S, int lengthS, int lengthT) {
     if ( lengthS == lengthT ){
         subStrings.push_back(S);
     } else {
-        for(int i = 0; i < (lengthS - lengthT + 1); i++) {
+        for(unsigned int i = 0; i < (lengthS - lengthT + 1); i++) {
             string temp = "";
-            cout<<"here"<<endl;
             for(int j = 0; j < lengthT; j++) {
-                cout<<j<<endl;
-                cout<<i+j<<endl;
-                temp[j] = S[i + j];
+                temp += S[i+j];
             }
             subStrings.push_back(temp);
         }
@@ -48,9 +52,9 @@ int totalDistance(string S, string T) {
 }
 
 int main() {
-    string S = "bababababababbababababababababababababbabababa";
-    string T = "bababababababbababababababababababababbababab";
-//    while(cin>> S >> T)
+    string S = "";
+    string T = "";
+    while(cin>> S >> T)
         cout << totalDistance(S,T) << endl;
-//    return 0;
+    return 0;
 }
