8f82a69201d0c700f5dccd4308741e85e6129e5f
WhatTheNathan <nathanliuyolo@gmail.com>
2018-03-30 23:30:54 +0800

    InversePairs

--- 剑指Offer/Arrays/InversePairs/main.cpp
+++ 剑指Offer/Arrays/InversePairs/main.cpp
@@ -6,31 +6,50 @@
 
 #include <iostream>
 
-using namespace::std;
-
-int inversePairs(int* nums ,int low, int high) {
-    if(!nums || low >= high)
+int inversePairsCore(int* nums, int* copy, int low, int high) {
+    if(low == high) {
+        copy[low] = nums[low];
         return 0;
-    int mid = (low + high) / 2;
-    int leftPairs = inversePairs(nums, low , mid);
-    int rightPairs = inversePairs(nums, mid + 1, high);
-    int count = 0;
-
-    int leftP = mid;
-
-    while(leftP >= low) {
-        for(int rightP = high; rightP >= mid + 1; rightP--) {
-            if(nums[leftP] > nums[rightP]){
-                count++;
-            }
-        }
-        leftP--;
     }
+    int mid = (low + high) / 2;
+    int leftPairs = inversePairsCore(copy,nums, low , mid);
+    int rightPairs = inversePairsCore(copy,nums, mid + 1, high);
+    int count = 0;
+    int leftP = mid;
+    int rightP = high;
+    int indexCopy = high;
+
+    while(leftP >= low && rightP >= mid+1) {
+        if(nums[leftP] > nums[rightP]) {
+            copy[indexCopy--] = nums[leftP--];
+            count += (rightP - mid);
+        }
+        else {
+            copy[indexCopy--] = nums[rightP--];
+        }
+    }
+    while(leftP>=low) { copy[indexCopy--] = nums[leftP--]; }
+    while(rightP>=mid+1) { copy[indexCopy--] = nums[rightP--]; }
     return leftPairs + rightPairs + count;
+}
+
+using namespace::std;
+int inversePairs(int* nums,int length) {
+    if(!nums || length < 0) {
+        return 0;
+    }
+    int* copy = new int[length];
+    for(int i = 0; i < length; i++) {
+        copy[i] = nums[i];
+    }
+    int count = inversePairsCore(nums,copy,0,length-1);
+    delete[] copy;
+    
+    return count;
 }
 
 int main(){
     const int length = 5;
     int nums[length] = {4,7,3,2,2};
-    cout<<inversePairs(nums,0,length - 1)<<endl;
+    cout<<inversePairs(nums,length)<<endl;
 }
