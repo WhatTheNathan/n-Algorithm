d04cc0ebcad4cadb0bc31ca6d8327fe5e55c3e48
WhatTheNathan <nathanliuyolo@gmail.com>
2018-04-01 15:14:02 +0800

    IsContinuous

--- 剑指Offer/Else/44.IsContinuous/main.cpp
+++ 剑指Offer/Else/44.IsContinuous/main.cpp
@@ -11,36 +11,35 @@ using namespace::std;
 
 // 大小王为0
 int poker[14] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
-int numbsers = 5;
+int length = 5;
 
 bool isContinuous(int* nums) {
     if(!nums)
         return false;
-    sort(nums,nums+numbsers);
+    sort(nums,nums+length);
     int numsOf0 = 0;
-    int minus = 0;
-    for(int i = 0; i < numbsers; i++) {
-        if(nums[i] == 0) {
-            numsOf0++;
-        }
-        if(i) {
-            // 有差值
-            if(nums[i] - nums[i-1] > 1 && nums[i-1] != 0) {
-                minus += 1;
-            }
-            // 有重复
-            if(nums[i] - nums[i-1] == 0 && nums[i] != 0) {
-                return false;
-            }
-        }
+    int numsOfGap = 0;
+
+    for(int i = 0; i < length && nums[i] == 0; i++) {
+        numsOf0++;
     }
-    if(minus == numsOf0 || minus == 0) {
-        return true;
+
+    int small = numsOf0;
+    int big = small + 1;
+
+    while(big < length) {
+        int gap = nums[big] - nums[small];
+        if(gap == 0) {
+            return false;
+        }
+        numsOfGap += (gap - 1);
+        small = big;
+        big++;
     }
-    return false;
+    return (numsOfGap > numsOf0) ? false : true;
 }
 
 int main() {
-    int test[5] = {5,6,8,7,4};
+    int test[5] = {0,2,4,5,6};
     cout<<isContinuous(test)<<endl;
 }
