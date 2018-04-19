29754a81f5dbf99dbfb69338de447edc9896b8a6
WhatTheNathan <nathanliuyolo@gmail.com>
2018-04-19 23:01:39 +0800

    Leecode 92

--- LeetCode/LinkedList/143.Reorder List/main.cpp
+++ LeetCode/LinkedList/143.Reorder List/main.cpp
@@ -11,43 +11,43 @@ struct ListNode {
 
 class Solution {
 public:
-    void reorderList(ListNode* head) {
-        if(!head || !head->next) {
-            return;
-        }
-        ListNode* pHead_1 = head;
-        ListNode* pHead_2 = head;
-        ListNode* temp = head->next;
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if(!head)
+            return nullptr;
+        if(m < n) {
+            ListNode* p = head;
+            ListNode* pp = nullptr;
+            ListNode* previous = nullptr;
+            int count = 1;
 
-        // find pHead_2 and divide
-        while(temp && temp->next) {
-            pHead_2 = pHead_2->next;
-            temp = temp->next->next;
-        }
-        temp = pHead_2;
-        pHead_2 = pHead_2->next;
-        temp->next = nullptr;
+            // move to start point
+            for( ; count < m; count++) {
+                if(!p)
+                    return head;
+                previous = p;
+                p = p->next;
+            }
+            ListNode* endPoint = p;
 
-        // reverse pList_2
-        ListNode* pp = nullptr;
-        while(pHead_2) {
-            temp = pp;
-            pp = pHead_2;
-            pHead_2 = pHead_2->next;
-            pp->next = temp;
-        }
-        pHead_2 = pp;
+            while(count < (n + 1)) {
+                if(!p)
+                    return head;
+                ListNode* temp = pp;
+                pp = p;
+                p = p->next;
+                pp->next = temp;
+                count++;
+            }
 
-        //merge 2 lists
-        while(pHead_1 && pHead_2) {
-            temp = pHead_1->next;
-            pHead_1->next = pHead_2;
-            pHead_1 = temp;
-
-            temp = pHead_2->next;
-            pHead_2->next = pHead_1;
-            pHead_2 = temp;
+            if(previous)
+                previous->next = pp;
+            else
+                head = pp;
+            endPoint->next = nullptr;
+            if(p)
+                endPoint->next = p;
         }
+        return head;
     }
 };
 
@@ -56,20 +56,21 @@ int main(){
     ListNode* node_2 = new ListNode(2);
     ListNode* node_3 = new ListNode(3);
     ListNode* node_4 = new ListNode(4);
-//    ListNode* node_5 = new ListNode(4);
+    ListNode* node_5 = new ListNode(5);
 //    ListNode* node_6 = new ListNode(4);
 //    ListNode* node_7 = new ListNode(5);
     node_1->next = node_2;
-    node_2->next = node_3;
-    node_3->next = node_4;
+//    node_2->next = node_3;
+//    node_3->next = node_4;
 //    node_4->next = node_5;
 //    node_5->next = node_6;
 //    node_6->next = node_7;
 
     Solution solution = Solution();
-    solution.reorderList(node_1);
-    while(node_1) {
-        cout<<node_1->val<<endl;
-        node_1 = node_1->next;
+    ListNode* node = solution.reverseBetween(node_1,1,2);
+//    solution.reorderList(node_1);
+    while(node) {
+        cout<<node->val<<endl;
+        node = node->next;
     }
 }
