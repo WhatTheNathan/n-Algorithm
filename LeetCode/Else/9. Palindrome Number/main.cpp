dd4c7e12e02ca1738af791ba06b46631fb3096db
WhatTheNathan <nathanliuyolo@gmail.com>
2018-04-25 19:47:12 +0800

    leetcode 9

---
+++ LeetCode/Else/9. Palindrome Number/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+
+using namespace::std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+
+        int start = 0;
+        int end = str.size() - 1;
+
+        while(start <= end) {
+            if(str[start++] != str[end--])
+                return false;
+        }
+        return true;
+    }
+};
+
+int main(){
+    Solution solution = Solution();
+    cout<<solution.isPalindrome(-12321)<<endl;
+}
